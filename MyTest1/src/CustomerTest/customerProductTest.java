5cc14db415a819bf967dfc8753b177a1bfee5274
mfcwl <mfcwl@DESKTOP-MO0FC5C>
2020-01-28 17:12:40 +0000

    commit msg

---
+++ MyTest1/src/CustomerTest/customerProductTest.java
@@ -0,0 +1,11 @@
+package CustomerTest;
+
+public class customerProductTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("product related Tc");
+	}
+
+}
